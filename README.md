460100a984b5f0781704edcd1efdfdebc72646c4
Leming Shen <zju.slm@gmail.com>
2022-09-04 17:29:22 +0800

    version 2.0

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Smart-Audio-Player
 
-This is an Android App that can play the specified sound from local disk. The timestamp in the format of `yyyy-MM-dd HH:mm:ss SSS` when you press the "PLAY" button will be logged on the screen for your information.
+This is an Android App that can play the specified sound from local disk and the chirp sound signal according to your configuration. The timestamp in the format of `yyyy-MM-dd HH:mm:ss SSS` when you press the "PLAY" button or the "CHIRP" button will be logged on the screen for your information.
 
 ## Basic Information
 
-Smart Audio Player has totally two functions.: playing the specified sound file and log the timestamp when you play the sound on the watch's screen. The timestamp will be in the format of `yyyy-MM-dd HH:mm:ss SSS` for your information. You can also add your sound file you want to play in the directory `/app/src/main/res/` and modity the code accordingly.
+Smart Audio Player has totally three functions: Playing the specified sound file and log the timestamp when you play the sound on the watch's screen; Playing the chirp sound signal according to your configuration. The timestamp will be in the format of `yyyy-MM-dd HH:mm:ss SSS` for your information. You can also add your sound file you want to play in the directory `/app/src/main/res/` and modity the code accordingly.
 
 ## How to Use
 
@@ -17,7 +17,8 @@ Smart Audio Player has totally two functions.: playing the specified sound file 
 
 ### 2. App Operation Tips
 
-- Start playing. Press the "PLAY" button and the watch will repeatedly play the specified sound file. Meanwhile, the timestamp when you click the button will be logged on the watch's screen.
+- Start playing file. Press the "PLAY" button and the watch will repeatedly play the specified sound file. Meanwhile, the timestamp when you click the button will be logged on the watch's screen.
+- Start playing chirp. Enter the start frequency, end frequency, and the duration of the chirp. Then press "CHIRP" button and the watch will repeatedly play the chirp sound signal.
 - Stop: Press the "Stop" button and the player will stop playing the sound.
 
 ## About us
--- app/src/main/java/com/example/smartaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/smartaudioplayer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.smartaudioplayer;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.smartaudioplayer.databinding.ActivityMainBinding;
 
@@ -20,14 +25,18 @@ public class MainActivity extends Activity
 	private ActivityMainBinding binding;
 	private ListView listView;
 	private SoundPool soundPool;
+	private AudioPlayer player;
 
 	@Override
+
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 
 		binding = ActivityMainBinding.inflate(getLayoutInflater());
 		setContentView(binding.getRoot());
+
+		player = new AudioPlayer();
 
 		/* bind the listview to local variable */
 		listView = binding.timeList;
@@ -41,7 +50,7 @@ public class MainActivity extends Activity
 		SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
 
 		/* update the time list when click the play button */
-		time_list.add(date_format.format(current_timestamp));
+		time_list.add("file " + date_format.format(current_timestamp));
 		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, time_list);
 		listView.setAdapter(adapter);
 
@@ -65,8 +74,151 @@ public class MainActivity extends Activity
 
 	public void stop_sound(View v)
 	{
-		soundPool.stop(1);
-		soundPool.release();
-		soundPool = null;
+		if (soundPool != null)
+		{
+			soundPool.stop(1);
+			soundPool.release();
+			soundPool = null;
+		}
+
+		player.stop_sound();
+	}
+
+	public void play_chirp(View v)
+	{
+		double start_frequency = Double.parseDouble(binding.startFrequency.getText().toString());
+		double end_frequency = Double.parseDouble(binding.endFrequency.getText().toString());
+		double duration = Double.parseDouble(binding.duration.getText().toString());
+
+		player.play_sound(start_frequency, end_frequency, 50, duration);
+		/* get the current timestamp */
+		long current_timestamp = System.currentTimeMillis();
+		SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+
+		/* update the time list when click the play button */
+		time_list.add("chirp " + date_format.format(current_timestamp));
+		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, time_list);
+		listView.setAdapter(adapter);
+
+//		Toast.makeText(MainActivity.this, "playing chirp sound", Toast.LENGTH_SHORT).show();
+	}
+}
+
+class AudioPlayer
+{
+	/* type of sound stream */
+	private static final int steam_type = AudioManager.STREAM_MUSIC;
+
+	/* specify the sample rate */
+	private static final int sample_rate = 44100;
+
+	/* double audio channel */
+	private static final int channel = AudioFormat.CHANNEL_OUT_STEREO;
+
+	/* specify digitalizing bit */
+	private static final int audio_format = AudioFormat.ENCODING_PCM_16BIT;
+
+	/* specify the buffer size */
+	private int mMinBufferSize;
+
+	/* stream mode */
+	private static int MODE = AudioTrack.MODE_STREAM;
+
+	/* using multi-thread */
+	private boolean isPlaying = false;
+	private thread_play playing_thread;
+
+	private AudioTrack track;
+	private int buffer_size;
+
+	/* initialization */
+	public AudioPlayer()
+	{
+		buffer_size = AudioTrack.getMinBufferSize(sample_rate, channel, audio_format);
+		this.track = new AudioTrack(steam_type, sample_rate, channel, audio_format, buffer_size, MODE);
+	}
+
+	/* playing chirp sound */
+	public void play_sound(double start_rate, double end_rate, int db, double duration)
+	{
+		this.playing_thread = new thread_play(start_rate, end_rate, db, duration);
+		playing_thread.start();
+	}
+
+	public void play(double start_rate, double end_rate, int db, double duration)
+	{
+		start_rate *= 1000;
+		end_rate *= 1000;
+
+		System.out.println(start_rate);
+		System.out.println(end_rate);
+
+		/* chirp generation */
+		int length = (int) (end_rate - start_rate) + 1;
+		int sample_per_frequency = (int) (sample_rate * duration / length);
+		int size = sample_per_frequency * length;
+		short amplitude = Short.MAX_VALUE;
+		short[] stream = new short[size];
+
+		amplitude = (short) ((Math.pow(10.0, db / 20.0) * Math.sqrt(2)) * amplitude);
+
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < sample_per_frequency; j++)
+			{
+				double x = 2.0 * Math.PI * i / 44100.0;
+				stream[i * sample_per_frequency + j] = (short) (amplitude * Math.sin(x * j));
+			}
+		}
+
+		this.track.play();
+		this.track.write(stream, 0, size);
+	}
+
+	/* using thread */
+	class thread_play extends Thread
+	{
+		private double start_rate, end_rate, duration;
+		private int db;
+
+		public thread_play(double start_rate, double end_rate, int db, double duration)
+		{
+			this.start_rate = start_rate;
+			this.end_rate = end_rate;
+			this.db = db;
+			this.duration = duration;
+		}
+
+		@Override
+		public void run()
+		{
+			try
+			{
+				if (!isPlaying)
+				{
+					isPlaying = true;
+
+					while (isPlaying)
+					{
+						play(this.start_rate, this.end_rate, this.db, duration);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void stop_sound()
+	{
+		this.isPlaying = false;
+		this.track.stop();
+	}
+
+	public void pause_sound()
+	{
+		this.track.pause();
 	}
 }
